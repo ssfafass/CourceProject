471dbd804286e6a42358c60348443e92ca7076ab
50cum2 <kolyat19961012@gmail.com>
2022-02-23 20:04:14 +0200

    Добавьте файлы проекта.

---
+++ CourceProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourceProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
